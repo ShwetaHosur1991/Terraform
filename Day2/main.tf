4c16119bdfcc7fe44d25bd63b8c1b3debcf2268b
ShwetaHosur1991 <hosur.tanvi@gmail.com>
2023-09-26 14:27:16 +0100

    Chaning the availabilty zone and the CIDR block

--- Day2/main.tf
+++ Day2/main.tf
@@ -1,34 +1,34 @@
 resource "aws_vpc" "my_vpc" {
     cidr_block = var.cidr 
 }
-resource "aws_subnet" "my_subnet1" {
+resource "aws_subnet" "my_sub1" {
   vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-west-1a"
-   map_public_ip_on_launch = true
-}
-resource "aws_subnet" "my_subnet2" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.102.0/24"
   availability_zone = "us-west-1b"
    map_public_ip_on_launch = true
 }
+resource "aws_subnet" "my_sub2" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.101.0/24"
+  availability_zone = "us-west-1c"
+   map_public_ip_on_launch = true
+}
 resource "aws_internet_gateway" "my_igw" {
-     vpc_id = aws_vpc.myvpc.id
+     vpc_id = aws_vpc.my_vpc.id
 }
 resource "aws_route_table" "my_route_table" {
-    vpc_id = aws_vpc.myvpc.id
+    vpc_id = aws_vpc.my_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.my_igw.id
     }
 }
 resource "aws_route_table_association" "my_rta1" {
-  subnet_id = aws_subnet.my_subnet1.id
+  subnet_id = aws_subnet.my_sub1.id
   route_table_id = aws_route_table.my_route_table.id
 }
 resource "aws_route_table_association" "my_rta2" {
-  subnet_id = aws_subnet.my_subnet2.id
+  subnet_id = aws_subnet.my_sub2.id
    route_table_id = aws_route_table.my_route_table.id
 }
 resource "aws_security_group" "sg" {
@@ -60,13 +60,13 @@ resource "aws_security_group" "sg" {
   }
 }
 resource "aws_s3_bucket" "my_s3" {
-  bucket = "shweta_terraform2023project"
+  bucket = "shwetaterraform2023project"
 }
 resource "aws_instance" "my_instance1_in_subnet1" {
     ami = "ami-06d2c6c1b5cbaee5f"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.sg.id]
-    subnet_id              = aws_subnet.sub1.id
+    subnet_id              = aws_subnet.my_sub1.id
     user_data              = base64encode(file("userdata.sh"))
   
 }
@@ -74,7 +74,7 @@ resource "aws_instance" "my_instance2_in_subnet2" {
     ami = "ami-06d2c6c1b5cbaee5f"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.sg.id]
-    subnet_id              = aws_subnet.sub2.id
+    subnet_id              = aws_subnet.my_sub2.id
     user_data              = base64encode(file("userdata.sh")) 
 }
 resource "aws_lb" "myalb" {
@@ -82,8 +82,8 @@ resource "aws_lb" "myalb" {
   internal           = false
   load_balancer_type = "application"
 
-  security_groups = [aws_security_group.webSg.id]
-  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+  security_groups = [aws_security_group.sg.id]
+  subnets         = [aws_subnet.my_sub1.id, aws_subnet.my_sub2.id]
 
   tags = {
     Name = "web"
@@ -94,7 +94,7 @@ resource "aws_lb_target_group" "tg" {
   name     = "myTG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id   = aws_vpc.my_vpc.id
 
   health_check {
     path = "/"
@@ -104,13 +104,13 @@ resource "aws_lb_target_group" "tg" {
 
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.webserver1.id
+  target_id        = aws_instance.my_instance1_in_subnet1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
   target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.webserver2.id
+  target_id        = aws_instance.my_instance2_in_subnet2.id
   port             = 80
 }
 
